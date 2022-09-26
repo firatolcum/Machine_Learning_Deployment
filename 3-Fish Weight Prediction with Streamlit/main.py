c7abd22f86db36695726f9e36dc3b41cf5e91c72
firatolcum <firatolcum@gmail.com>
2022-09-26 18:07:42 +0300

    3-Fish Weight Prediction with Streamlit update/

--- 3-Fish Weight Prediction with Streamlit/main.py
+++ 3-Fish Weight Prediction with Streamlit/main.py
@@ -4,9 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import  LabelEncoder
 import xgboost as xgb
 import numpy as np
-st.header("Fish Weight Prediction App")
-st.text_input("Enter your Name: ", key="name")
-data = pd.read_csv("https://raw.githubusercontent.com/gurokeretcha/WishWeightPredictionApplication/master/Fish.csv")
+
+html_temp = """
+<div style="background-color:Blue;padding:1.5px">
+<h1 style="color:white;text-align:center;">Select Fish Features</h1>
+</div><br>"""
+st.sidebar.markdown(html_temp,unsafe_allow_html=True)
+
+html_temp2 = """
+<div style="background-color:blue;padding:1.5px">
+<h1 style="color:white;text-align:center;">Fish Wight Prediction App</h1>
+</div><br>"""
+st.markdown(html_temp2,unsafe_allow_html=True)
+
+data = pd.read_csv("Fish.csv")
 #load label encoder
 encoder = LabelEncoder()
 encoder.classes_ = np.load('classes.npy',allow_pickle=True)
@@ -16,21 +27,15 @@ best_xgboost_model = xgb.XGBRegressor()
 best_xgboost_model.load_model("best_model.json")
 
 if st.checkbox('Show Training Dataframe'):
-    data
-
-st.subheader("Please select relevant features of your fish!")
-left_column, right_column = st.columns(2)
-with left_column:
-    inp_species = st.radio(
-        'Name of the fish:',
-        np.unique(data['Species']))
+    data[:5]
 
 
-input_Length1 = st.slider('Vertical length(cm)', 0.0, max(data["Length1"]), 1.0)
-input_Length2 = st.slider('Diagonal length(cm)', 0.0, max(data["Length2"]), 1.0)
-input_Length3 = st.slider('Cross length(cm)', 0.0, max(data["Length3"]), 1.0)
-input_Height = st.slider('Height(cm)', 0.0, max(data["Height"]), 1.0)
-input_Width = st.slider('Diagonal width(cm)', 0.0, max(data["Width"]), 1.0)
+inp_species = st.sidebar.selectbox('Kind of the fish:', np.unique(data['Species']))
+input_Length1 = st.sidebar.number_input('Vertical length(cm)', 0.0, max(data["Length1"]), .0,)
+input_Length2 = st.sidebar.number_input('Diagonal length(cm)', 0.0, max(data["Length2"]), .0)
+input_Length3 = st.sidebar.number_input('Cross length(cm)', 0.0, max(data["Length3"]), .0)
+input_Height = st.sidebar.number_input('Height(cm)', 0.0, max(data["Height"]), .0)
+input_Width = st.sidebar.number_input('Diagonal width(cm)', 0.0, max(data["Width"]), .0)
 
 
 if st.button('Make Prediction'):
@@ -38,11 +43,7 @@ if st.button('Make Prediction'):
     inputs = np.expand_dims(
         [int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
     prediction = best_xgboost_model.predict(inputs)
-    print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Your fish weight is: {np.squeeze(prediction, -1):.2f}g")
-
-    st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
-    st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
+    st.success(f"Your fish weight is: {np.squeeze(prediction, -1):.2f} g")
 
 
 
